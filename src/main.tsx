d2c1d6bb9972b55daad993a8a66270672ea283ba
Hoi Nam <hoi.nam@iddy.ai>
2024-07-07 13:25:46 +0800

    add src

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// this manifest is used temporarily for development purposes
+const manifestUrl = "https://nhynhyiddy.github.io/my-twa/tonconnect-manifest.json";
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,
+  </TonConnectUIProvider>,
 )
